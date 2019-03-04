018cc240a3909cb39e6b1836a422109f170202f7
Zboubizte <ken.bres@hotmail.fr>
2019-03-04 15:19:18 +0100

    Chargement des metadonnées et lecture audio

--- app/src/main/java/com/example/mewsick/MainActivity.java
+++ app/src/main/java/com/example/mewsick/MainActivity.java
@@ -3,9 +3,16 @@ package com.example.mewsick;
 import android.Manifest;
 import android.app.Activity;
 import android.content.pm.PackageManager;
+import android.content.res.AssetFileDescriptor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.Image;
+import android.media.MediaMetadataRetriever;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -19,6 +26,8 @@ import com.jackandphantom.blurimage.BlurImage;
 import java.io.File;
 import java.io.IOException;
 import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 import edu.cmu.pocketsphinx.Assets;
 import edu.cmu.pocketsphinx.Hypothesis;
@@ -34,6 +43,8 @@ public class MainActivity extends Activity implements RecognitionListener
 	private String NGRAM_SEARCH = "salut";
 	private boolean enEcoute = false;
 
+	private Handler durationHandler = new Handler();
+
 	TextView artisteAlbum;
 	TextView morceau;
 	TextView tempsActuel;
@@ -42,27 +53,35 @@ public class MainActivity extends Activity implements RecognitionListener
 	ImageView enregistrer;
 	SeekBar progress;
 
+	Bitmap image;
+	String titre, artiste, album;
+	float duree, dureePassee;
+
+	MediaPlayer mp;
+	MediaMetadataRetriever mmr = null;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		artisteAlbum = findViewById(R.id.artisteAlbum);
-		morceau = findViewById(R.id.morceau);
-		tempsActuel = findViewById(R.id.tempsActuel);
-		tempsTotal = findViewById(R.id.tempsTotal);
-		albumArt = findViewById(R.id.albumArt);
-		enregistrer = findViewById(R.id.enregistrer);
-		progress = findViewById(R.id.progress);
+		checkPermissions();
+		linkUI();
+		loadSong(R.raw.strawberry_girls_betelgeuse);
+		blurBG();
 
-		int permissionCheck = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.RECORD_AUDIO);
-
-		if (permissionCheck != PackageManager.PERMISSION_GRANTED)
+		albumArt.setOnClickListener(new View.OnClickListener()
 		{
-			ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, PERMISSIONS_REQUEST_RECORD_AUDIO);
-			return;
-		}
+			@Override
+			public void onClick(View v)
+			{
+				if (mp.isPlaying())
+					pause();
+				else
+					jouer();
+			}
+		});
 
 		enregistrer.setOnClickListener(new View.OnClickListener()
 		{
@@ -70,23 +89,93 @@ public class MainActivity extends Activity implements RecognitionListener
 			public void onClick(View v)
 			{
 				if (!enEcoute)
-				{
 					ecouter();
-				}
 				else
-				{
 					stop();
-				}
 			}
 		});
 
+		new SetupTask(this).execute();
+	}
+
+	private void blurBG()
+	{
 		BlurImage.with(getApplicationContext())
-				.load(R.drawable.placeholder)
-				.intensity(100)
+				.load(image)
+				.intensity(50)
 				.Async(true)
 				.into((ImageView) findViewById(R.id.backgroundImg));
+	}
 
-		new SetupTask(this).execute();
+	private void checkPermissions()
+	{
+		int permissionCheck = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.RECORD_AUDIO);
+
+		if (permissionCheck != PackageManager.PERMISSION_GRANTED)
+		{
+			ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, PERMISSIONS_REQUEST_RECORD_AUDIO);
+			return;
+		}
+	}
+
+	private void linkUI()
+	{
+		artisteAlbum = findViewById(R.id.artisteAlbum);
+		morceau = findViewById(R.id.morceau);
+		tempsActuel = findViewById(R.id.tempsActuel);
+		tempsTotal = findViewById(R.id.tempsTotal);
+		albumArt = findViewById(R.id.albumArt);
+		enregistrer = findViewById(R.id.enregistrer);
+		progress = findViewById(R.id.progress);
+	}
+
+	private void loadSong(int fichier)
+	{
+		mp = MediaPlayer.create(this, fichier);
+		mmr = new MediaMetadataRetriever();
+
+		final AssetFileDescriptor afd = getResources().openRawResourceFd(fichier);
+		mmr.setDataSource(afd.getFileDescriptor(),afd.getStartOffset(),afd.getLength());
+
+		titre = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE);
+		artiste = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ARTIST).toUpperCase();
+		album = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ALBUM).toUpperCase();
+
+		byte[] data = mmr.getEmbeddedPicture();
+		image = BitmapFactory.decodeByteArray(data, 0, data.length);
+
+		artisteAlbum.setText(artiste + " - " + album);
+		morceau.setText(titre);
+		albumArt.setImageBitmap(image);
+		tempsTotal.setText(String.format("%02d:%02d", (mp.getDuration() / 1000) / 60, (mp.getDuration() / 1000) % 60));
+
+		progress.setMax(mp.getDuration());
+		progress.setClickable(false);
+	}
+
+	private void jouer()
+	{
+		mp.start();
+		progress.setProgress((int) mp.getCurrentPosition());
+		durationHandler.postDelayed(updateSeekBarTime, 100);
+	}
+
+	private Runnable updateSeekBarTime = new Runnable()
+	{
+		public void run()
+		{
+			dureePassee = mp.getCurrentPosition();
+			progress.setProgress((int) dureePassee);
+			double timeRemaining = duree - dureePassee;
+			tempsActuel.setText(String.format("%02d:%02d", TimeUnit.MILLISECONDS.toMinutes((long) timeRemaining), TimeUnit.MILLISECONDS.toSeconds((long) timeRemaining) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes((long) timeRemaining))));
+
+			durationHandler.postDelayed(this, 100);
+		}
+	};
+
+	private void pause()
+	{
+		mp.pause();
 	}
 
 	private static class SetupTask extends AsyncTask<Void, Void, Exception>
